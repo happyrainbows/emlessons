726d73e826a44b146c4f9f006826717e2a4034e7
Andy <andyfazliu@hotmail.com>
2022-10-25 12:56:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState, useEffect, Fragment} from 'react';
+
 function App() {
+  const [biologicalSex, setbiologicalSex] = useState(null)
+
+
+  const getBiologicalSex = async () => {
+    setbiologicalSex("Male");
+  }
+
+  const renderFirstContainer = () => {
+    return (
+      <div className="App">
+        <p>hi, are you a male?</p>
+        <button onClick={getBiologicalSex}> Male </button>
+
+      </div>);    
+    }
+
+
+  const renderMaleContainer = () => {
+    return (
+      <div className="App">
+        <p>Congratulations you are a {biologicalSex}</p>
+      </div>);    
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {biologicalSex == null && renderFirstContainer()}
+      {biologicalSex==="Male" && renderMaleContainer()}
     </div>
   );
 }
