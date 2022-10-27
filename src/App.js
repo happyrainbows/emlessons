8b638261feb13be6c3a853b1ed82f12e48766218
Emily  Morris <emilymorris@emilys-imac.lan>
2022-10-27 13:15:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,52 @@ import React, {useState, useEffect, Fragment} from 'react';
 
 function App() {
   const [biologicalSex, setbiologicalSex] = useState(null)
+  const [open, setOpen] = React.useState(false);
 
-
-  const getBiologicalSex = async () => {
+  const handleOpen = () => {
+    setOpen(!open);
+   
+  };
+  const getSex = async () => {
+    setbiologicalSex("curiousgeorge");
+  }
+  const getMaleSex = async () => {
     setbiologicalSex("Male");
   }
-
+  const getFemaleSex = async () => {
+    setbiologicalSex("Female");
+  }
+  
   const renderFirstContainer = () => {
     return (
       <div className="App">
         <p>hi, are you a male?</p>
-        <button onClick={getBiologicalSex}> Male </button>
-
+        <button onClick={getMaleSex}> Male </button>
+        <button onClick={getFemaleSex}> Female </button>
+       
+     
       </div>);    
     }
-
+    const renderFirstFirstContainer = () => {
+      return (
+        <div className="App">
+          <div className="dropdown">
+        <button onClick={handleOpen}>Genetic Disorder</button>
+        {open ? (
+          <ul className="menu">
+            <li className="menu-item">
+              <button onClick={getSex}>Cystic Fibrosis</button>
+            </li>
+            <li className="menu-item">
+              <button onClick={getSex}>Tay-Sachs</button>
+            </li>
+          </ul>
+        ) : null}
+        {open ? <div>Is Open</div> : <div>Is Closed</div>}
+      </div>
+       
+        </div>);    
+      }
 
   const renderMaleContainer = () => {
     return (
@@ -27,11 +58,17 @@ function App() {
         <p>Congratulations you are a {biologicalSex}</p>
       </div>);    
     }
-
+  const renderFemaleContainer = () => {
+     return (
+       <div className="App">
+          <p>Congratulations you are a {biologicalSex}</p>
+        </div>);    
+      }
 
   return (
     <div className="App">
-      {biologicalSex == null && renderFirstContainer()}
+      {biologicalSex==null && renderFirstFirstContainer()}
+      {biologicalSex === "curiousgeorge" && renderFirstContainer()}
       {biologicalSex==="Male" && renderMaleContainer()}
     </div>
   );
